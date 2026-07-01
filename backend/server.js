a00ea3145c49014340777901ca8002c197e38713
Mamadou-shop <gneidmamadou17@gmail.com>
2026-07-01 03:00:43 +0000

    fix: force lowercase config path for Linux

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./Config/Db");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
